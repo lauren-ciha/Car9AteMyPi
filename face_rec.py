d83a69971dbb42237a55d945eb168bdb8d8dd057
mallorymalvitz <mallorymalvitz@gmail.com>
2023-11-12 11:46:11 -0600

    adding mallory pic

--- face_rec.py
+++ face_rec.py
@@ -13,15 +13,19 @@ import numpy as np
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
-#
+
 # Load a sample picture and learn how to recognize it.
+mallory_image = face_recognition.load_image_file("mallory_pic.png")
+mallory_face_encoding = face_recognition.face_encodings(mallory_image)[0]
 casey_image = face_recognition.load_image_file("casey_face.png")
 casey_face_encoding = face_recognition.face_encodings(casey_image)[0]
 # Create arrays of known face encodings and their names
 known_face_encodings = [
+    mallory_face_encoding,
     casey_face_encoding
 ]
 known_face_names = [
+    "Mallory",
     "Casey"
 ]
 
@@ -94,52 +98,3 @@ while True:
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
-#
-# class FaceRecognizer:
-#
-#     def __init__(self, face_imgs, face_labels):
-#         # Load a sample picture and learn how to recognize it.
-#         self.face_images = []
-#         self.known_face_encodings = []
-#         self.known_face_names = []
-#         for face_img, label in zip(face_imgs, face_labels):
-#             self.face_images.append(face_recognition.load_image_file(face_img))
-#             self.known_face_encodings.append(face_recognition.face_encodings(self.face_images[-1])[0])
-#             self.known_face_names.append(label)
-#
-#         # Initialize some variables
-#         face_locations = []
-#         face_encodings = []
-#         face_names = []
-#
-#     def find_locs_given_frame(self, frame):
-#         # Resize frame of video to 1/4 size for faster face recognition processing
-#         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-#
-#         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-#         rgb_small_frame = small_frame[:, :, ::-1]
-#
-#         # Find all the faces and face encodings in the current frame of video
-#         face_locations = face_recognition.face_locations(rgb_small_frame)
-#         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-#
-#         face_names = []
-#         for face_encoding in face_encodings:
-#             # See if the face is a match for the known face(s)
-#             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-#             name = "Unknown"
-#
-#             # # If a match was found in known_face_encodings, just use the first one.
-#             # if True in matches:
-#             #     first_match_index = matches.index(True)
-#             #     name = known_face_names[first_match_index]
-#
-#             # Or instead, use the known face with the smallest distance to the new face
-#             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-#             best_match_index = np.argmin(face_distances)
-#             if matches[best_match_index]:
-#                 name = self.known_face_names[best_match_index]
-#
-#             face_names.append(name)
-#
-#         return face_locations, face_names
